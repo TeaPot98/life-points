4948e2557aac95aafe470f9af5a4cfdb360391fe
TeaPot98 <trifautan@gmail.com>
2026-08-20 21:23:30 +0200

    start defining entities types

---
+++ types/index.ts
@@ -0,0 +1,53 @@
+interface Activity {
+  name: string;
+  /**
+   * Link to icon location
+   */
+  icon: string;
+  color: string;
+  reward: number;
+}
+
+interface ReadingTracker {
+  latestBook: Book;
+  books: Book[];
+  rewardPerPage: number;
+}
+
+interface Book {
+  title: string;
+  author: string;
+  numberOfPages: number;
+  readPages: number;
+}
+
+interface TimeActivity extends Activity {
+  type: ActivityType.Time;
+}
+
+interface MilestoneActivity extends Activity {
+  type: ActivityType.Milestone;
+  milestones: Milestone[];
+}
+
+interface CountActivity extends Activity {
+  type: ActivityType.Count;
+}
+
+interface Milestone {
+  name: string;
+  reward: number;
+}
+
+interface TimeActivityInstance extends TimeActivity {
+  /**
+   * Duration in minutes
+   */
+  duration: number;
+}
+
+enum ActivityType {
+  Milestone,
+  Time,
+  Count,
+}
